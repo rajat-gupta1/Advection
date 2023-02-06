8bfc5b18f88c34b764b78094756d104ece597442
Rajat Gupta <rajatgupta@linux6.cs.uchicago.edu>
2023-02-05 23:46:47 -0600

    submit

---
+++ project1final.c
@@ -0,0 +1,309 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <assert.h>
+#include "mpi.h"
+#include <omp.h>
+
+int main(int argc, char *argv[])
+{
+    int nprocs, mype;
+    MPI_Status stat;
+    int order = 1;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size (MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank (MPI_COMM_WORLD, &mype);
+
+    int N, NT, n1, nt, method;
+    double L, T, u, v;
+
+    if (mype == 0)
+    {
+        N = atoi(argv[1]);
+        NT = atoi(argv[2]);
+        L = atof(argv[3]);
+        T = atof(argv[4]);
+        u = atof(argv[5]);
+        v = atof(argv[6]);
+        nt = atoi(argv[7]);
+        method = atoi(argv[8]);
+        // Printing the inputs given by the user
+        printf("The value of Matrix Dimension, N is:                  %i\n", N);
+        printf("The value of Number of timestamps, NT is:             %i\n", NT);
+        printf("The value of Physical Cartesian Domain Length, L is:  %f\n", L);
+        printf("The value of Total Physical Timespan, T is:           %f\n", T);
+        printf("The value of X velocity scalar, u is:                 %.10f\n", u);
+        printf("The value of Y velocity scalar, v is:                 %.10f\n", v);
+        int mem = N * N * 2;
+        printf("Memory to be used will be %d time size of double\n", mem);
+
+        MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&NT, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&L, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&T, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&u, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&v, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&nt, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&method, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    }
+
+    else
+    {
+        MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&NT, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&L, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&T, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&u, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&v, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&nt, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&method, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
+    if (method == 3)
+        order = 2;
+    omp_set_num_threads(nt);
+
+    double delx = L / N;
+    double delt = T / NT;
+
+    assert(delt <= delx / sqrt(2 * (pow(u, 2) + pow(v, 2))));
+    
+    double x0 = L / 2;
+    double y0 = x0;
+
+    double sigx = L / 4;
+    double sigy = sigx;
+
+    n1 = sqrt(nprocs);
+    int new_N = N / n1;
+
+    int start_j = new_N * (mype % n1);
+    int start_i = mype / n1;
+    start_i *= new_N;  
+
+    // double mCn[new_N + order * 2][new_N + order * 2];
+    // double mCn1[new_N + order * 2][new_N + order * 2];
+
+    double *ptr;
+    double *ptr1;
+    double **mCn;
+    double **mCn1, **temp;
+    int len = sizeof(double *) * (new_N + order * 2) + sizeof(double) * (new_N + order * 2) * (new_N + order * 2);
+    mCn = (double **)malloc(len);
+    mCn1 = (double **)malloc(len);
+    ptr = (double *)(mCn + new_N + order * 2);
+    ptr1 = (double *)(mCn1 + new_N + order * 2);
+
+    for (int i = 0; i < new_N + order * 2; i++)
+    {
+        mCn[i] = (ptr + (new_N + order * 2) * i);
+        mCn1[i] = (ptr1 + (new_N + order * 2) * i);
+    }
+
+    // printf("%i\n", mype);
+    for (int i = order; i < new_N + order; i++)
+    {
+        for (int j = order; j < new_N + order; j++)
+        {
+            mCn[i][j] = exp(
+                -(
+                    pow((i - order + start_i) * delx - x0, 2) / (2 * pow(sigx, 2)) + 
+                    pow((j - order + start_j) * delx - y0 ,2) / (2 * pow(sigy, 2))
+                )
+            );
+            // printf("%f ", mCn[i][j]);
+        }
+        // printf("\n");
+    }
+
+    // MPI_Barrier(MPI_COMM_WORLD);
+
+    double for_send_top[(new_N + order * 2) * order];
+    double for_send_bot[(new_N + order * 2) * order];
+    double for_send_lef[(new_N + order * 2) * order];
+    double for_send_rig[(new_N + order * 2) * order];
+
+    double t1 = omp_get_wtime();
+    
+    for (int n = 0; n < NT; n++)
+    {
+        
+        if ((mype % n1 + mype / n1) % 2 == 1)
+        {   
+            MPI_Recv(for_send_lef, (new_N + order * 2) * order, MPI_DOUBLE, ((mype) % n1 ? mype - 1 : mype + n1 - 1) , 1, MPI_COMM_WORLD, &stat);
+            MPI_Recv(for_send_rig, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + 1) % n1 ? mype + 1 : mype - n1 + 1) , 1, MPI_COMM_WORLD, &stat);
+            MPI_Recv(for_send_top, (new_N + order * 2) * order, MPI_DOUBLE, ((mype - n1) >= 0 ? mype - n1 : mype + nprocs - n1) , 1, MPI_COMM_WORLD, &stat);
+            MPI_Recv(for_send_bot, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + n1) < nprocs ? mype + n1 : mype - nprocs + n1) , 1, MPI_COMM_WORLD, &stat);     
+
+            #ifdef PARALLEL
+            #pragma omp parallel for default(none) shared(for_send_lef, for_send_rig, for_send_bot, for_send_top, n1, new_N, order, temp, mype, nprocs, N, NT, L, T, u, v, method, mCn, mCn1, delx, delt, x0, y0, sigx, sigy, n) schedule(static)
+            #endif
+            for (int i = order; i < (new_N + order * 2) * order - 1; i++)
+            {
+                mCn[i / (new_N + order * 2)][i % ((new_N + order * 2))] = for_send_top[i];
+                mCn[i / (new_N + order * 2) + (new_N + order)][i % ((new_N + order * 2))] = for_send_bot[i];
+                mCn[i % ((new_N + order * 2))][i / (new_N + order * 2)] = for_send_lef[i];
+                mCn[i % ((new_N + order * 2))][i / (new_N + order * 2) + (new_N + order)] = for_send_rig[i];
+                for_send_lef[i] = mCn[i % ((new_N + order * 2))][order + i / (new_N + order * 2)];
+                for_send_rig[i] = mCn[i % ((new_N + order * 2))][i / (new_N + order * 2) + (new_N)];
+                for_send_top[i] = mCn[order + i / (new_N + order * 2)][i % ((new_N + order * 2))];
+                for_send_bot[i] = mCn[i / (new_N + order * 2) + (new_N)][i % ((new_N + order * 2))];
+            }
+            MPI_Send(for_send_bot, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + n1) < nprocs ? mype + n1 : mype - nprocs + n1) , 1, MPI_COMM_WORLD);
+            MPI_Send(for_send_top, (new_N + order * 2) * order, MPI_DOUBLE, ((mype - n1) >= 0 ? mype - n1 : mype + nprocs - n1), 1, MPI_COMM_WORLD);
+            MPI_Send(for_send_rig, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + 1)  % n1 ? mype + 1 : mype - n1 + 1), 1, MPI_COMM_WORLD);
+            MPI_Send(for_send_lef, (new_N + order * 2) * order, MPI_DOUBLE, ((mype) % n1 ? mype - 1 : mype + n1 - 1), 1, MPI_COMM_WORLD);
+            
+        }
+        else
+        // if (mype == 0)
+        {
+            #ifdef PARALLEL
+            #pragma omp parallel for default(none) shared(for_send_lef, for_send_rig, for_send_bot, for_send_top, n1, new_N, order, temp, mype, nprocs, N, NT, L, T, u, v, method, mCn, mCn1, delx, delt, x0, y0, sigx, sigy, n) schedule(static)
+            #endif
+            for (int i = order; i < (new_N + order * 2) * order - 1; i++)
+            {
+                for_send_lef[i] = mCn[i % ((new_N + order * 2))][order + i / (new_N + order * 2)];
+                for_send_rig[i] = mCn[i % ((new_N + order * 2))][i / (new_N + order * 2) + (new_N)];
+                for_send_top[i] = mCn[order + i / (new_N + order * 2)][i % ((new_N + order * 2))];
+                for_send_bot[i] = mCn[i / (new_N + order * 2) + (new_N)][i % ((new_N + order * 2))];
+            }
+            MPI_Send(for_send_bot, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + n1) < nprocs ? mype + n1 : mype - nprocs + n1) , 1, MPI_COMM_WORLD);
+            MPI_Send(for_send_top, (new_N + order * 2) * order, MPI_DOUBLE, ((mype - n1) >= 0 ? mype - n1 : mype + nprocs - n1), 1, MPI_COMM_WORLD);
+            MPI_Send(for_send_rig, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + 1)  % n1 ? mype + 1 : mype - n1 + 1), 1, MPI_COMM_WORLD);
+            MPI_Send(for_send_lef, (new_N + order * 2) * order, MPI_DOUBLE, ((mype) % n1 ? mype - 1 : mype + n1 - 1), 1, MPI_COMM_WORLD);
+            MPI_Recv(for_send_lef, (new_N + order * 2) * order, MPI_DOUBLE, ((mype) % n1 ? mype - 1 : mype + n1 - 1) , 1, MPI_COMM_WORLD, &stat);
+            MPI_Recv(for_send_rig, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + 1)  % n1 ? mype + 1 : mype - n1 + 1) , 1, MPI_COMM_WORLD, &stat);
+            MPI_Recv(for_send_top, (new_N + order * 2) * order, MPI_DOUBLE, ((mype - n1) >= 0 ? mype - n1 : mype + nprocs - n1) , 1, MPI_COMM_WORLD, &stat);
+            MPI_Recv(for_send_bot, (new_N + order * 2) * order, MPI_DOUBLE, ((mype + n1) < nprocs ? mype + n1 : mype - nprocs + n1) , 1, MPI_COMM_WORLD, &stat);
+
+            #ifdef PARALLEL
+            #pragma omp parallel for default(none) shared(for_send_lef, for_send_rig, for_send_bot, for_send_top, n1, new_N, order, temp, mype, nprocs, N, NT, L, T, u, v, method, mCn, mCn1, delx, delt, x0, y0, sigx, sigy, n) schedule(static)
+            #endif
+            for (int i = order; i < (new_N + order * 2) * order - 1; i++)
+            {
+                mCn[i / (new_N + order * 2)][i % ((new_N + order * 2))] = for_send_top[i];
+                mCn[i / (new_N + order * 2) + (new_N + order)][i % ((new_N + order * 2))] = for_send_bot[i];
+                mCn[i % ((new_N + order * 2))][i / (new_N + order * 2)] = for_send_lef[i];
+                mCn[i % ((new_N + order * 2))][i / (new_N + order * 2) + (new_N + order)] = for_send_rig[i];
+            }
+        }
+
+        // printf("%i\n", mype);
+        // for (int i = 0 ; i <(new_N + order * 2); i++)
+        // {
+        //     for (int j = 0 ; j <(new_N + order * 2); j++)
+        //         printf("%f ", mCn[i][j]);
+        //     printf("\n");
+        // }
+
+        #ifdef PARALLEL
+        #pragma omp parallel for default(none) shared(n1, new_N, order, temp, mype, nprocs, N, NT, L, T, u, v, method, mCn, mCn1, delx, delt, x0, y0, sigx, sigy, n) schedule(static)
+        #endif
+        for (int i = order; i < new_N + order; i++)
+        {
+            for (int j = order; j < new_N + order; j++)
+            {
+                // The boundary conditions. Wrapping around whenever encountering a boundary
+                int im = i - 1;
+                int imm = i - 2;
+                int ip = i + 1;
+                int ipp = i + 2;
+                int jm = j - 1;
+                int jmm = j - 2;
+                int jp = j + 1;
+                int jpp = j + 2;
+
+                if (method == 1)
+                    mCn1[i][j] = 1.0 / 4.0 * (mCn[im][j] + mCn[ip][j] + mCn[i][jm] + mCn[i][jp]) - delt / (2.0 * delx) * (u * (mCn[ip][j] - mCn[im][j]) + v * (mCn[i][jp] - mCn[i][jm]));
+
+                else if (method == 2)
+                {
+                    if (u > 0 && v > 0)
+                        mCn1[i][j] = mCn[i][j] - delt / delx * (u * (mCn[i][j] - mCn[im][j]) + v * (mCn[i][j] - mCn[i][jm]));
+                    else
+                        mCn1[i][j] = mCn[i][j] - delt / delx * (u * (mCn[ip][j] - mCn[i][j]) + v * (mCn[i][jp] - mCn[i][j]));
+                }
+
+                else if (method == 3)
+                {
+                    if (u > 0 && v > 0)
+                        mCn1[i][j] = mCn[i][j] - delt / (2.0 * delx) * (u * (3.0 * mCn[i][j] - 4.0 * mCn[im][j] + mCn[imm][j]) + (v * (3.0 * mCn[i][j] - 4.0 * mCn[i][jm] + mCn[i][jmm])));
+                    else
+                        mCn1[i][j] = mCn[i][j] + delt / (2.0 * delx) * (u * (3.0 * mCn[i][j] - 4.0 * mCn[ip][j] + mCn[ipp][j]) + (v * (3.0 * mCn[i][j] - 4.0 * mCn[i][jp] + mCn[i][jpp])));
+                }
+            }
+        }
+
+        // Storing value of Cn1 to Cn. Swapping it to ensure that we dont lose reference to the pointer of Cn1
+        // for (int i = 0 + order; i < new_N + order; i++)
+        //     for (int j = 0 + order; j < new_N + order; j++)
+        //         mCn[i][j] = mCn1[i][j];
+        temp = mCn1;
+        mCn1 = mCn;
+        mCn = temp;
+    }
+
+    double t2 = omp_get_wtime();
+    printf("time(s): %f\n", t2 - t1);
+
+    if (mype == 0)
+    {
+        double **Cn = (double**)malloc(N * sizeof(double*));
+
+        for (int i = 0; i < N; i++)
+            Cn[i] = (double*)malloc(N * sizeof(double));
+
+        
+        for (int k = 0; k < new_N; k++)
+            for (int j = 0; j < new_N; j++)
+                Cn[k][j] = mCn1[k + order][j + order];
+
+        for (int i = 1; i < nprocs; i++)
+        {
+            MPI_Recv(&mCn1[0][0], pow((new_N + order * 2), 2), MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &stat);
+            int start_j = new_N * (i % n1);
+            int start_i = i / n1;
+            start_i *= new_N; 
+
+            for (int k = 0; k < new_N; k++)
+                for (int j = 0; j < new_N; j++)
+                    Cn[k + start_i][j + start_j] = mCn1[k + order][j + order];
+        }
+
+        // for (int i = 0; i < N; i++)
+        // {
+        //     for (int j = 0; j < N; j++)
+        //         printf("%f ", Cn[i][j]);
+        //     printf("\n");
+        // }
+
+        for (int i = 0; i < N; i++)
+            free(Cn[i]);
+
+        free(Cn);
+    }
+    
+    else
+        MPI_Send(&mCn1[0][0], pow((new_N + order * 2), 2), MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+    
+    // printf("%i\n", mype);
+    // for (int i = 0 + order; i < new_N + order; i++)
+    // {
+    //     for (int j = 0 + order; j < new_N + order; j++)
+    //     {
+    //         printf("%f ", mCn1[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    free(mCn);
+    free(mCn1);
+
+    MPI_Finalize();
+
+    return 0;
+}
